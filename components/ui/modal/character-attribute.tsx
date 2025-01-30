52df07cf2470752a09f5b74e67f03e7b17aa6f16
kyle.lan <kylel@nfcc.cc>
2025-01-30 17:40:18 +0800

    feat: Finish character attribute modal design

--- components/ui/modal/character-attribute.tsx
+++ components/ui/modal/character-attribute.tsx
@@ -6,6 +6,11 @@ import { RxCross2 } from "react-icons/rx"
 import { BiPlusMedical } from "react-icons/bi"
 import { getPlayerByClass } from "@/lib/api/players"
 import { Player } from "@/lib/api/players/type"
+import { FaHeart } from "react-icons/fa"
+import { GiSpiralBottle } from "react-icons/gi"
+import { PiSwordDuotone } from "react-icons/pi"
+import { TbShieldCheckeredFilled } from "react-icons/tb"
+import { PiCoinsFill } from "react-icons/pi"
 
 type CharacterAttributeProps = {
     setIsShowCharacterAttributeModal: React.Dispatch<React.SetStateAction<boolean>>
@@ -35,8 +40,8 @@ function CharacterAttribute({ setIsShowCharacterAttributeModal, character }: Cha
 
     return (
         <BaseModal onClick={() => setIsShowCharacterAttributeModal(false)}>
-            <div className="absolute left-1/2 top-1/2 flex h-full max-h-[90%] w-full max-w-xs -translate-x-1/2 -translate-y-1/2 flex-col items-center rounded-2xl bg-neutral-700 md:max-w-xl">
-                <div className="flex w-full items-center justify-between rounded-t-2xl bg-neutral-800 px-4 py-3">
+            <div className="absolute left-1/2 top-1/2 flex w-full max-w-xs -translate-x-1/2 -translate-y-1/2 flex-col items-center rounded-2xl bg-neutral-200">
+                <div className="flex w-full items-center justify-between rounded-t-2xl bg-neutral-500 px-4 py-3">
                     <BiPlusMedical className="invisible text-xl" />
                     <span className="text-center text-xl font-bold">{character.characterClass}</span>
                     <RxCross2
@@ -53,15 +58,36 @@ function CharacterAttribute({ setIsShowCharacterAttributeModal, character }: Cha
                         height={250}
                     />
                 </div>
-                <div className="text-sm font-medium text-neutral-400">Allocate Ability Points</div>
-                <div className="p-3">
+                <div className="text-md font-semibold text-neutral-800">Character Ability Points</div>
+                <div className="mb-8 p-3">
                     {!loading && characterAttribute && (
-                        <div
-                            key={characterAttribute._id}
-                            className="flex items-center justify-between rounded-lg border border-neutral-600 bg-neutral-900 p-3"
-                        >
-                            <div className="text-base font-normal text-neutral-50">
-                                {characterAttribute.characterClass}
+                        <div className="flex w-[180px] flex-col items-start justify-between rounded-lg border border-neutral-600 bg-neutral-900 py-3 pl-5 pr-8">
+                            <div className="flex items-center justify-between">
+                                <FaHeart className="mr-2 text-base font-normal text-red-500" />
+                                <span className="mr-3 text-base font-normal text-neutral-50">HP:</span>
+                                <div className="text-base font-normal text-neutral-50">{characterAttribute.hp}</div>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <GiSpiralBottle className="mr-2 text-base font-normal text-blue-500" />
+                                <span className="mr-3 text-base font-normal text-neutral-50">MP:</span>
+                                <div className="text-base font-normal text-neutral-50">{characterAttribute.mp}</div>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <PiSwordDuotone className="mr-2 text-base font-normal text-white" />
+                                <span className="mr-3 text-base font-normal text-neutral-50">Attack:</span>
+                                <div className="text-base font-normal text-neutral-50">{characterAttribute.attack}</div>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <TbShieldCheckeredFilled className="mr-2 text-base font-normal text-white" />
+                                <span className="mr-3 text-base font-normal text-neutral-50">Defense:</span>
+                                <div className="text-base font-normal text-neutral-50">
+                                    {characterAttribute.defense}
+                                </div>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <PiCoinsFill className="mr-2 text-base font-normal text-yellow-500" />
+                                <span className="mr-3 text-base font-normal text-neutral-50">Gold:</span>
+                                <div className="text-base font-normal text-neutral-50">{characterAttribute.gold}</div>
                             </div>
                         </div>
                     )}
