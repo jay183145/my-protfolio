2dd383be41b91a5578b472c88516defb3f06e98c
kyle.lan <kylel@nfcc.cc>
2025-02-02 11:18:56 +0800

    feat: Add loading screen to character select page

--- app/character-select/page.tsx
+++ app/character-select/page.tsx
@@ -1,11 +1,14 @@
 import CharacterSelect from "@/app/character-select/components/character-select-section"
-import React from "react"
+import React, { Suspense } from "react"
+import ScreenLoading from "../lodaing"
 
 const CharacterSelectPage: React.FC = () => {
     return (
-        <div className="flex h-full max-h-screen w-full flex-col items-center justify-center px-10 py-5">
-            <CharacterSelect />
-        </div>
+        <Suspense fallback={<ScreenLoading />}>
+            <div className="flex h-full max-h-screen w-full flex-col items-center justify-center px-10 py-5">
+                <CharacterSelect />
+            </div>
+        </Suspense>
     )
 }
 
